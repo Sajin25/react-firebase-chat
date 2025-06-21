8e3d59524382e7f4d76f992906e47745fcce2067
Sajin25 <120718410+Sajin25@users.noreply.github.com>
2025-06-22 00:21:47 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,49 @@
 
+import { Link } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
+import { AppBar, Toolbar, Button, Typography, Box } from '@mui/material';
+
+const Navbar = () => {
+  const { user, logOut } = UserAuth();
+
+  const handleLogout = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Chat App
+        </Typography>
+        <Box>
+          {user ? (
+            <>
+              <Button color="inherit" component={Link} to="/rooms">
+                Rooms
+              </Button>
+              <Button color="inherit" onClick={handleLogout}>
+                Logout
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button color="inherit" component={Link} to="/login">
+                Login
+              </Button>
+              <Button color="inherit" component={Link} to="/signup">
+                Sign Up
+              </Button>
+            </>
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
