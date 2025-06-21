e70e6be19210e3ba9f89b819053004b0e30ebc0c
Sajin25 <120718410+Sajin25@users.noreply.github.com>
2025-06-22 00:19:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,42 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthContextProvider } from './context/AuthContext';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Login from './components/Auth/Login';
+import Signup from './components/Auth/Signup';
+import RoomList from './components/Chat/RoomList';
+import ChatRoom from './components/Chat/ChatRoom';
+import ProtectedRoute from './components/ProtectedRoute';
 
+function App() {
+  return (
+    <Router>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route 
+            path="/rooms" 
+            element={
+              <ProtectedRoute>
+                <RoomList />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/rooms/:roomId" 
+            element={
+              <ProtectedRoute>
+                <ChatRoom />
+              </ProtectedRoute>
+            } 
+          />
+        </Routes>
+      </AuthContextProvider>
+    </Router>
+  );
+}
+
+export default App;
